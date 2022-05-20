c2697da369b530624b78fa328ce8888bddfb982a
namphan22 <phanvanhoainam22@gmail.com>
2022-05-20 20:53:32 +0700

    done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 #include<string>
 #include<fstream>
 #include<stdint.h>
+#include<conio.h>
 using namespace std;
+void gotoxy(int x,int y)
+
+{
+	// x is a length , y is a width
+    COORD coordinates;
+    coordinates.X =x;
+    coordinates.Y =y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coordinates);
+}
+void setup_color(int value_color)
+{
+     WORD Color;
+                         
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+                        
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+                   
+          Color = (csbi.wAttributes & 0xF0) + (value_color & 0x0F);
+          SetConsoleTextAttribute(hStdOut, Color);
+     }
+    
+}
 void question_part(void)
 {
     string question[50];
@@ -13,8 +39,8 @@ void question_part(void)
     uint16_t count;
     
     fstream File;
-    File.open("question_user.txt",ios::in);// mở file ở chế độ đọc
-    while(!File.eof()) // đọc tới cuối file trả lại giá trị true
+    File.open("question_user.txt",ios::in);// m? file ? ch? d? d?c
+    while(!File.eof()) // d?c t?i cu?i file tr? l?i giá tr? true
     {
         getline(File,question[num]);
         getline(File,ans[num++]);
@@ -22,7 +48,7 @@ void question_part(void)
        
     }
     File.close();
-    cout<<num;
+    
     
     for(count =0;count <num-1;count++)
     {
@@ -38,12 +64,50 @@ void question_part(void)
         } while (answer_user!=ans[count]);
         
     }
- 
-  
+ }
+ void pre_process(void)
+ {
+ 	char animation1 = 177;
+ 	char animation2 = 219;
+    setup_color(10);
+    gotoxy(50,10);
+    cout<<"Loading...";
+    gotoxy(42,11);
+    uint8_t i;
+    for(i=0;i<25;i++)
+    {
+    	cout<<animation1;
+	}
+	gotoxy(42,11);
+	for(i=0;i<25;i++)
+	{
+		cout<<animation2;
+		Sleep(300);
+	}
+	system("color F");
+	system("cls");
+	
+     
+     
+     
 
- 
-}
+ }
+ void image_ascii(void)
+ {
+ 	fstream outf;
+ 	outf.open("picture.txt",ios ::in);
+    string p;
+    while (getline(outf,p))
+    {
+        cout<<p<<endl;
+        Sleep(100);
+    }
+    outf.close();
+    
+ }
 int main()
 {
     question_part();
+    pre_process();
+    image_ascii();
 }
